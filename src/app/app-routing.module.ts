9827f95a56bcb86e202cabdd223ba01c4e953ff9
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-23 14:44:33 +0200

    Fix import for profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProfileComponent } from './components/profile/profile/profile.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { AuthGuard } from './guards/auth.guard';
 
 
