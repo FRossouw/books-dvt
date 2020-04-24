aceafa8c1c188b67979c6d122ef7bdd1a3157418
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-24 14:28:16 +0200

    Add book model and unit test

---
+++ src/app/models/book.ts
@@ -0,0 +1,17 @@
+export class Book {
+    isbN10: string;
+    isbN13: string;
+    title: string;
+    about: string;
+    abstract: string;
+    author: {
+        href: string;
+        id: string;
+        name: string;
+    }
+    publisher: string;
+    datePublished: Date;
+    image: string;
+    tags: string[];
+    version: string;
+}
