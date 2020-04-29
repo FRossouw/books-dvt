40fe7c7d1e3eafd73fec390f7c145772fa8266d3
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-29 09:17:07 +0200

    Unit test all models

---
+++ src/app/models/book-author.spec.ts
@@ -0,0 +1,16 @@
+import { BookAuthor } from './book-author';
+
+describe('BookAuthor', () => {
+  it('should create an instance of a Book Author', () => {
+    expect(new BookAuthor()).toBeTruthy();
+  });
+
+  it('should create a Book Author with data', () => {
+    const author = new BookAuthor;
+    author.href = "www.localhost.co.za.png";
+    author.id = "1700";
+
+    expect(author).toBeTruthy();
+  });
+
+});
