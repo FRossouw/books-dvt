3c244cb1dbc0787d76d399a4019b576580ebdb2a
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-29 14:43:45 +0200

    Add author component

---
+++ src/app/components/author/author.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthorService } from 'src/app/services/author.service';
+import { Author } from 'src/app/models/author';
+
+@Component({
+  selector: 'app-author',
+  templateUrl: './author.component.html',
+  styleUrls: ['./author.component.scss']
+})
+export class AuthorComponent implements OnInit {
+
+  authors: Author[] = new Array();
+
+  constructor(private authorService: AuthorService) { }
+
+  ngOnInit(): void {
+    this.getAuthors();
+    console.log(this.authors);
+  }
+
+  private getAuthors(): void {
+    this.authors = new Array();
+    this.authorService.getAuthors().subscribe((authorX) => {
+      authorX.forEach((authorFE) => {
+        this.authors.push(authorFE);
+      });
+    });
+
+  }
+
+}
