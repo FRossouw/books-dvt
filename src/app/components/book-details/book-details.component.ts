910740d1303ab369378c895b1064d2b3c947de6a
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-05-02 16:08:27 +0200

    Add book details component

--- src/app/components/book-details/book-details.component.ts
+++ src/app/components/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from 'src/app/models/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-book-details',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
+
+  constructor(private activatedRoute: ActivatedRoute, public bookService: BookService) { }
 
   ngOnInit(): void {
+    let bookIsbn13: string;
+    this.activatedRoute.paramMap.subscribe(params => {
+      bookIsbn13 = params.get('id');
+    });
+
+    this.getBook(bookIsbn13);
+
+  }
+
+  private getBook(isbn13: string): void {
+    this.book = new Book();
+    this.bookService.getBook(isbn13).subscribe((bookX) => {
+      console.log(bookX)
+      this.book = bookX;
+    });
   }
 
 }
