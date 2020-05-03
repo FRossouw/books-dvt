abb96e9dc51d0c3015c54361a3fa2b47461cef88
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-05-03 11:05:18 +0200

    Fix failing unit test tailing whitespace

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -10,14 +10,14 @@ import { environment } from 'src/environments/environment';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<boolean> | Promise<boolean|UrlTree> | boolean {
+  ): Observable<boolean> | Promise<boolean | UrlTree> | boolean {
     return this.auth.isAuthenticated$.pipe(
-      tap(loggedIn => {        
+      tap(loggedIn => {
         if (this.auth.userProfileData[environment.namespace] && this.auth.userProfileData[environment.namespace].includes('admin')) {
           this.auth.admin = true;
           this.auth.user = false;
