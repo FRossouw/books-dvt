8fb01194e8d5be6da1f023b1dec780b48a9298c6
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-05-03 10:53:05 +0200

    Fix failing unit test trailing whitespace

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -17,8 +17,7 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean|UrlTree> | boolean {
     return this.auth.isAuthenticated$.pipe(
-      tap(loggedIn => {
-        
+      tap(loggedIn => {        
         if (this.auth.userProfileData[environment.namespace] && this.auth.userProfileData[environment.namespace].includes('admin')) {
           this.auth.admin = true;
           this.auth.user = false;
