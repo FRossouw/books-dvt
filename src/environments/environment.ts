04f057e944b65f5e76b6dccd44286881f9febd19
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-05-03 10:33:49 +0200

    Fix namespace variable in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiBooks: 'http://localhost:4201/Books',
   apiAuthors: 'http://localhost:4201/Authors',
   apiTags: 'http://localhost:4201/Tags',
-  namespace: 'http://localhost:4201/role'
+  namespace: 'http://localhost:4200/claims/role'
 };
 
 /*
