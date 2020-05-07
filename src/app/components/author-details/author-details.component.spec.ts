f6306afaafeb875cff22857b97dd69bca1cf34ff
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-05-07 10:25:13 +0200

    Unit test author details

--- src/app/components/author-details/author-details.component.spec.ts
+++ src/app/components/author-details/author-details.component.spec.ts
@@ -4,13 +4,28 @@ import { AuthorDetailsComponent } from './author-details.component';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
+import { Author } from 'src/app/models/author';
+import { AuthService } from 'src/app/services/auth.service';
+import { AuthorService } from 'src/app/services/author.service';
+
+class MockService {
+  getAuthors(): Observable<Author[]> {
+    return {} as Observable<Author[]>;
+  }
+}
+
+class MockAuth { }
+
+class mockActivatedRoute {}
 
 describe('AuthorDetailsComponent', () => {
   let component: AuthorDetailsComponent;
   let fixture: ComponentFixture<AuthorDetailsComponent>;
+  let service: MockService;
+
   const mockActivatedRoute = {
-    paramMap: of({ get: () => '372dc0d0-6368-4eb3-8876-8b20d07cf722' })
+    paramMap: of({ get: (id) => id = '372dc0d0-6368-4eb3-8876-8b20d07cf722' })
   };
 
   beforeEach(async(() => {
@@ -21,6 +36,14 @@ describe('AuthorDetailsComponent', () => {
         RouterTestingModule
       ],
       providers: [
+        {
+          AuthService,
+          useValue: MockAuth
+        },
+        {
+          AuthorService,
+          useValue: AuthorService
+        },
         {
           provide: ActivatedRoute,
           useValue: mockActivatedRoute
@@ -34,9 +57,10 @@ describe('AuthorDetailsComponent', () => {
     fixture = TestBed.createComponent(AuthorDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = new MockService();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
