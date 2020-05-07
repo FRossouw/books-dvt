bcb4811b75fea81901c85bffb23a71327d427686
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-05-07 11:19:23 +0200

    Unit test author form component

--- src/app/components/forms/author-form/author-form.component.spec.ts
+++ src/app/components/forms/author-form/author-form.component.spec.ts
@@ -1,25 +1,56 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthorFormComponent } from './author-form.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { AuthorService } from 'src/app/services/author.service';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+class MockService {
+
+}
 
 describe('AuthorFormComponent', () => {
   let component: AuthorFormComponent;
   let fixture: ComponentFixture<AuthorFormComponent>;
+  let service: MockService;
+  let httpTestingController: HttpTestingController;
+
+  const mockActivatedRoute = {
+    paramMap: of({ get: (id) => id = '372dc0d0-6368-4eb3-8876-8b20d07cf722' })
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthorFormComponent ]
+      declarations: [AuthorFormComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute
+        },
+        {
+          AuthorService,
+          useValue: MockService
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AuthorFormComponent);
+    httpTestingController = TestBed.inject(HttpTestingController);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = new MockService();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    component.getAuthor = () => { };
+    component.updateAuthor = () => { };
+    component.addAuthor = () => { };
+    expect(component).toBeTruthy();
+  });
 });
