92a0f5e9edc0547ee5c72dc879a54baa635c6007
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-05-07 15:05:41 +0200

    Add unit tests

--- src/app/components/forms/author-form/author-form.component.spec.ts
+++ src/app/components/forms/author-form/author-form.component.spec.ts
@@ -7,9 +7,22 @@ import { AuthorService } from 'src/app/services/author.service';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthorReturn } from 'src/app/models/author-return';
+import { AuthorBook } from 'src/app/models/author-book';
+import { Author } from 'src/app/models/author';
 
 class MockService {
   createAuthor(): Observable<AuthorReturn> { return new Observable<AuthorReturn>(); }
+  getAuthor(): Author {
+    return {
+      href: "http://localhost:4201/Authors/d9636037-fb42-4885-9890-1f4375f3ef6c",
+      id: "d9636037-fb42-4885-9890-1f4375f3ef6c",
+      first_name: "Tom",
+      last_name: "Rudderham",
+      name: "Tom  Rudderham",
+      about: "Tom kicked off his writing career at Future.",
+      version: "AAAAAAAAEHI="
+    } as Author
+  }
 }
 
 describe('AuthorFormComponent', () => {
@@ -67,13 +80,26 @@ describe('AuthorFormComponent', () => {
 
   });
 
-  it('when form submitted, book should check that the addAuthor method is called', () => {
+  it('when form submitted, author should check that the addAuthor method is called', () => {
     component.addAuthor = () => { };
     component.update = true;
     component.saveForm();
     const spy = spyOn(component, 'addAuthor').and.callThrough();
     spy();
     expect(spy).toHaveBeenCalled();
+
+  });
+
+  it('when form submitted, set form values when an author is returned ', () => {
+    fixture.detectChanges();
+    component.form.controls.firstName.setValue('Tom');
+    component.form.controls.middleName.setValue('Middle');
+    component.form.controls.lastName.setValue('Seller');
+    component.form.controls.about.setValue('About');
+    expect(component.form.controls.firstName.value).toEqual('Tom');
+    expect(component.form.controls.middleName.value).toEqual('Middle');
+    expect(component.form.controls.lastName.value).toEqual('Seller');
+    expect(component.form.controls.about.value).toEqual('About');
 
   });
 
